68f9a4522802fe63957bde3986a8e16376a9d973
zeen2o <bayrout2k1x3@outlook.com>
2025-09-25 23:11:18 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # This script downloads only the page lists for every single category.
 
-echo "###  APP(LEVEL 1) ###"
-python downloader_ultimate.py apps --all-pages --fetch-slugs --fetch-links
+echo "### APP (LEVEL 1) ###"
+python3 downloader_ultimate.py apps --all-pages --fetch-slugs --fetch-links
 
-ECHO ###   GAME(LEVEL 1) ###"
+echo "### GAME (LEVEL 1) ###"
+python3 downloader_ultimate.py games --all-pages --fetch-slugs --fetch-links
 
-python downloader_ultimate.py games --all-pages --fetch-slugs --fetch-links
-
-ECHO ### ALL TASKS COMPLETE ###
-pause
+echo "### ALL TASKS COMPLETE ###"
