b79d59ec28ff775a8ac22917f0cb485de8421287
zeen2o <bayrout2k1x3@outlook.com>
2025-09-27 18:25:30 +0100

    Delete cm.bat

--- cm.bat
+++
@@ -1,6 +0,0 @@
-"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" ^
-    --code="4/0AVGzR1DncqBdXcQAnYbgvNK_NXBD6bDZZWpajZQMUFqLYfKKbpdqaWlJ6S2KzcH8s40vbQ" ^
-    --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
-    --name=%COMPUTERNAME% ^
-    --pin=123456
-exit
