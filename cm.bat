f270cc08e8163a776e33a08d1e7ce7352f6e01c7
zeen2o <bayrout2k1x3@outlook.com>
2025-09-27 17:08:56 +0100

    Update and rename ChReDp.bat to cm.bat

---
+++ cm.bat
@@ -0,0 +1,6 @@
+"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" ^
+    --code="4/0Ab_5qlnBMYB1uQQlbHdTPmnSvFQQuJiMRVgGdKYEDKL9AIm4xcV6btvvjYn4XTRgjgLRGA" ^
+    --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
+    --name=%COMPUTERNAME% ^
+    --pin=123456
+exit
